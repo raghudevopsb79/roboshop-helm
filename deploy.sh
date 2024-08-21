782c67dee706849a0e6f5964aba5f7feb08ba208
Raghu K <raghuk.vit@gmail.com>
2024-08-21 07:50:27 +0530

    RoboShop Terraform & Kubernetes Code

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+if [ -z "$1" ]; then
+  echo APP_NAME input is missing
+  echo Usage: $0 APP_NAME ENV NAMESPACE
+  exit 1
+else
+  APP_NAME=$1
+fi
+
+if [ -z "$2" ]; then
+  echo ENV input is missing
+  echo Usage: $0 APP_NAME ENV NAMESPACE
+  exit 1
+else
+  ENV=$2
+fi
+
+if [ -z "$3" ]; then
+  echo NAMESPACE input is missing
+  echo Usage: $0 APP_NAME ENV NAMESPACE
+  exit 1
+else
+  ENV=$2
+fi
+
+ARGOCD_PASSWORD=$(kubectl get secrets argocd-initial-admin-secret -n argocd  -o=jsonpath='{.data.password}' | base64 --decode)
+argocd login  argocd-main-dev.rdevopsb79.online:443 --grpc-web --username admin --password ${ARGOCD_PASSWORD}
+
