fc0ff50b2976b72887198ceeaacf0c6cf014e315
Raghu K <raghuk.vit@gmail.com>
2024-08-21 07:56:24 +0530

    RoboShop Terraform & Kubernetes Code

--- deploy.sh
+++ deploy.sh
@@ -25,3 +25,6 @@ fi
 ARGOCD_PASSWORD=$(kubectl get secrets argocd-initial-admin-secret -n argocd  -o=jsonpath='{.data.password}' | base64 --decode)
 argocd login  argocd-main-dev.rdevopsb79.online:443 --grpc-web --username admin --password ${ARGOCD_PASSWORD}
 
+argocd app create ${APP_NAME} --project default --sync-policy auto --repo https://github.com/raghudevopsb79/roboshop-helm --path chart --dest-namespace ${NAMESPACE} --dest-server //kubernetes.default.svc --values-literal-file env-${ENV}/${APP_NAME}.yaml
+
+
