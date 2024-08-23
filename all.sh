cfeca53853d0b9af71da338bac5117830f8fdb5e
Raghu K <raghuk.vit@gmail.com>
2024-08-23 07:47:05 +0530

    RoboShop Terraform & Kubernetes Code

---
+++ all.sh
@@ -0,0 +1,6 @@
+bash deploy.sh catalogue dev app
+bash deploy.sh cart dev app
+bash deploy.sh user dev app
+bash deploy.sh shipping dev app
+bash deploy.sh payment dev app
+bash deploy.sh frontend dev web
