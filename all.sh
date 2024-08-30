9d01f5ae7e3ac8602b18c010f8874547f2ae916d
Raghu K <raghuk.vit@gmail.com>
2024-08-30 08:45:40 +0530

    RoboShop Terraform & Kubernetes Code

--- all.sh
+++ all.sh
@@ -1,6 +1,19 @@
-bash deploy.sh catalogue dev app
-bash deploy.sh cart dev app
-bash deploy.sh user dev app
-bash deploy.sh shipping dev app
-bash deploy.sh payment dev app
-bash deploy.sh frontend dev web
+case $1 in
+dev)
+    bash deploy.sh catalogue dev app
+    bash deploy.sh cart dev app
+    bash deploy.sh user dev app
+    bash deploy.sh shipping dev app
+    bash deploy.sh payment dev app
+    bash deploy.sh frontend dev web
+    ;;
+prod)
+    bash deploy.sh catalogue prod app
+    bash deploy.sh cart prod app
+    bash deploy.sh user prod app
+    bash deploy.sh shipping prod app
+    bash deploy.sh payment prod app
+    bash deploy.sh frontend prod web
+    ;;
+esac
+
